20eafe02e00b5d6d42c689c89299828f5738a05b
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-05-15 09:49:04 +0600

    db setup

--- components/layout/RightSideBar.jsx
+++ components/layout/RightSideBar.jsx
@@ -1,7 +1,14 @@
 const RightSideBar = () => {
   return (
-    <aside className="h-screen flex flex-col gap-5 sticky top-0 left-0 overflow-auto custom-scrollbar px-10 py-6 max-md:hidden">
-      RightSideBar
+    <aside className="h-screen flex flex-col gap-12 sticky top-0 left-0 overflow-auto custom-scrollbar px-10 py-6 max-md:hidden w-[300px] xl:w-[350px]">
+      <div>
+        <h1 className="text-heading4-bold mb-4">Following</h1>
+        <p>user list</p>
+      </div>
+      <div>
+        <h1 className="text-heading4-bold mb-4">Suggested People</h1>
+        <p>user list</p>
+      </div>
     </aside>
   );
 };
