210bccd4594834d2af8eab820232738e14660e5c
fs-nirjhor <fsnirjhor2002@gmail.com>
2024-05-20 15:54:50 +0600

    protect route middleware

--- components/layout/Profile.jsx
+++ components/layout/Profile.jsx
@@ -24,33 +24,32 @@ const Profile = () => {
   useEffect(() => {
     getUser();
   }, [userId]);
-  const { firstname, lastname, profilePhoto, followers, following, posts } =
-    user;
+
   return !isLoaded || isLoading ? (
     <Loading />
   ) : (
     <section className="flex flex-col gap-2">
       <Image
-        src={profilePhoto}
+        src={user?.profilePhoto}
         alt="avatar"
         width={50}
         height={50}
         className="rounded-full mx-auto"
       />
-      <p className="text-small-bold text-light-1 text-center">{`${firstname} ${lastname}`}</p>
+      <p className="text-small-bold text-light-1 text-center">{`${user?.firstname} ${user?.lastname}`}</p>
       <div className="flex justify-between text-center">
         <div>
-          <span className="text-base-bold">{posts?.length}</span>
+          <span className="text-base-bold">{user?.posts?.length}</span>
           <br />
           <span className="text-tiny-medium">Posts</span>
         </div>
         <div>
-          <span className="text-base-bold">{followers?.length}</span>
+          <span className="text-base-bold">{user?.followers?.length}</span>
           <br />
           <span className="text-tiny-medium">Followers</span>
         </div>
         <div>
-          <span className="text-base-bold">{following?.length}</span>
+          <span className="text-base-bold">{user?.following?.length}</span>
           <br />
           <span className="text-tiny-medium">Following</span>
         </div>
